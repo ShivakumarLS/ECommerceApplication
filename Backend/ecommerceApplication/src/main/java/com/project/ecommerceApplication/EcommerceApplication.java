b4b41b8244596aaf72c735bbdb70d101282843c3
Shivakumar L S <76564722+ShivakumarLS@users.noreply.github.com>
2024-03-31 08:56:56 +0530

    Add files via upload
    backend

---
+++ Backend/ecommerceApplication/src/main/java/com/project/ecommerceApplication/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecommerceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
---
+++ Backend/ecommerceApplication/src/main/java/com/project/ecommerceApplication/repository/ProductCategoryRepository.java
@@ -0,0 +1,15 @@
+package com.project.ecommerceApplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.project.ecommerceApplication.entity.ProductCategory;
+
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin("http://localhost:4200")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long >{
+
+
+}
